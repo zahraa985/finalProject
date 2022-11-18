09f217dc135cdb97d5d139e94115d704a5681320
zahraa985 <zaa19650@gmail.com>
2022-11-17 19:57:21 -0800

    fixed section#3

--- section#3/integration.py
+++ section#3/integration.py
@@ -1,47 +1,15 @@
-from numpy import linspace , sum, exp, sin, cos, pi
-from scipy.interpolate import CubicSpline
-import matplotlib.pyplot as plt
+def team5Integrate(f, a, b):
+    A1 = -((a-b)*(3*a-b))/(6*(7*a-3*b))
+    A2 = (4*(a-b)**2)/(3*(a+3*b))
+    A3 = -(4*(a-b)**2)/(3*(7*a-3*b))
+    A4 = -((a-b)*(11*a+b))/(6*(a+3*b))
 
-plt.style.use('seaborn-poster')
+    return A1*f(a) + A2*f((b-a)/4) + A3*f((3*(b-a))/4) + A4*f(b)
 
-class integration:
+f = lambda x: 1
+g = lambda x: x
+h = lambda x: x**2
 
-    def cubic(f, a, b, n):
-        if n%2 ==0:
-            h = float(b - a)/n
-            #h2 = 2*h; ah = a + h
-            result = f(a)+f(b)
-            for i in range(1,n):
-                xe = a + i*h 
-                if i%2 == 0:
-                    result = result + 2 * f(xe)
-                else:
-                    result = result + 3 * f(xe)
-
-            # Finding final integration value
-            result = result * 3 * h / 8
-            
-        else:
-            print("n has to be even numbers")
-            result= None
-        return result    
-
-
-f = lambda x: x**2
-F = lambda x: (x**3)/3
-a, b = -1, 2
-
-exact = F(b) - F(a)
-nvalues = [2**i for i in range(1,7)]
-
-
-
-
-print("\t%s\t\t \t\t%s\t\t  \n" %("n", "Cubic Intergration Algorithm"))
-
-for n in nvalues:
-    
-    errsim=abs(exact-integration.cubic(f, a, b, n))
-    #print(f"# h = {(b-a)/n}, Cubic Intergration error = {round(errsim,6)}")
-    print(n, integration.cubic(f, a, b, n))
-
+print(team5Integrate(f, 0, 1))
+print(team5Integrate(g, 0, 1))
+print(team5Integrate(h, 0, 1))
